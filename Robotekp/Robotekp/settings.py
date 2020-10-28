7d5605a5903fd4b8bc430d936e7bd6bdb66e958f
Axriuz <63169445+Axriuz@users.noreply.github.com>
2020-10-27 18:09:21 -0600

    Add files via upload

--- Robotekp/Robotekp/settings.py
+++ Robotekp/Robotekp/settings.py
@@ -16,6 +16,10 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -65,7 +69,7 @@ WSGI_APPLICATION = 'Robotekp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Robotekp',
+        'NAME': 'Robotekpdb',
         'USER': 'root',
         'PASSWORD': 'sistemas',
         'HOST':'',
@@ -109,9 +113,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 
